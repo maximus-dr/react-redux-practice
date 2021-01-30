c7cd66b81cfdc4c385b7491a4fbe5450e6afe57b
Max Ivanov <maximus-dr@mail.ru>
2021-01-30 18:11:42 +0300

    Configured fetching data from server in Home component via axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { Component } from 'react';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <h4 className="center">Home Page</h4>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quod pariatur delectus eius quam repellendus totam quos deserunt eveniet maxime cumque corrupti vel quia quis nemo, asperiores doloremque in libero?
-      </p>
-    </div>
-  )
+
+export default class Home extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          posts: response.data.slice(0, 10)
+        })
+      })
+  }
+
+  render() {
+
+    const { posts } = this.state;
+    const postsList = posts.length
+      ? posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+      : <div className="center">No posts yet</div>
+
+    return (
+      <div className="container">
+        <h4 className="center">Home Page</h4>
+        {postsList}
+      </div>
+    );
+  }
 }
