65af4f1a9f8f1bc1404b070377806d86ceca4c3d
Max Ivanov <maximus-dr@mail.ru>
2021-01-30 17:14:39 +0300

    Replaced links in Navbar with Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
 
-export default function Navbar() {
+function Navbar(props) {
+  console.log(props);
+  
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
         <a className="brand-logo">Time's</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
       </div>
     </nav>
   )
 }
+
+export default withRouter(Navbar);
