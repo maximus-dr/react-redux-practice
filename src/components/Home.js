5dab13e9f7affda3fbfc08c9ec83c4b02e3d0891
Max Ivanov <maximus-dr@mail.ru>
2021-01-31 13:12:49 +0300

    Configured routing between Home, About, Contact, Post pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 export default class Home extends Component {
@@ -11,7 +12,6 @@ export default class Home extends Component {
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response);
         this.setState({
           posts: response.data.slice(0, 10)
         })
@@ -25,10 +25,12 @@ export default class Home extends Component {
       ? posts.map(post => {
         return (
           <div className="post card" key={post.id}>
-            <div className="card-content">
-              <span className="card-title">{post.title}</span>
-              <p>{post.body}</p>
-            </div>
+            <Link to={`/${post.id}`}>
+              <div className="card-content">
+                <span className="card-title">{post.title}</span>
+                <p>{post.body}</p>
+              </div>
+            </Link>
           </div>
         );
       })
